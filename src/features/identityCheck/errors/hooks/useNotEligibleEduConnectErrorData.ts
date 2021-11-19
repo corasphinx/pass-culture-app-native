039b039aec95c92a0f21e0fa601f210f326e0b89
Wendy Jean-Louis <jeanlouis.wendy@gmail.com>
2021-11-19 16:09:04 +0100

    (PC-11651) Create InvalidAgeFromEduConnect Error

--- src/features/identityCheck/errors/hooks/useNotEligibleEduConnectErrorData.ts
+++ src/features/identityCheck/errors/hooks/useNotEligibleEduConnectErrorData.ts
@@ -1,3 +1,4 @@
+import { t } from '@lingui/macro'
 import { FunctionComponent } from 'react'
 import { TextStyle } from 'react-native'
 
@@ -14,8 +15,8 @@ type NotEligibleEduConnectErrorData = {
 }
 const UserAgeNotValid: NotEligibleEduConnectErrorData = {
   Icon: Clock,
-  title: `Tu ne fais pas partie de la phase de test`,
-  description: `Le pass Culture pour les jeunes de 15 à 17 ans est actuellement en phase de test auprès de 22 établissements scolaires des académies de Rennes et de Versailles.
+  title: t`Tu ne fais pas partie de la phase de test`,
+  description: t`Le pass Culture pour les jeunes de 15 à 17 ans est actuellement en phase de test auprès de 22 établissements scolaires des académies de Rennes et de Versailles.
 
 Encore un peu de patience... On se donne rendez-vous en janvier 2022 : nous reviendrons vers toi dès que le pass te sera accessible.
 
@@ -26,18 +27,27 @@ En attendant, tu peux tout de même découvrir l'application mais sans pouvoir r
 
 const InvalidAgeFromEduConnect: NotEligibleEduConnectErrorData = {
   Icon: InfoFraud,
-  title: `Oh Non!`,
-  description: `La date de naissance enregistrée dans ÉduConnect semble indiquer que tu n'as pas l'âge requis pour obtenir l'aide du Gouvernement.
+  title: t`Oh Non!`,
+  description: t`La date de naissance enregistrée dans ÉduConnect semble indiquer que tu n'as pas l'âge requis pour obtenir l'aide du Gouvernement.
 
 S’il y a une erreur sur ta date de naissance, contacte ton établissement pour modifier ton profil ÉduConnect.`,
   titleAlignment: 'center',
   descriptionAlignment: 'center',
 }
 
+const InvalidInformation: NotEligibleEduConnectErrorData = {
+  Icon: InfoFraud,
+  title: t`Oh Non!`,
+  description: t`Il semblerait que les informations que tu nous as communiquées ne soient pas correctes.
+ 
+Refais une demande en vérifiant ton identité avec ta pièce d’identité.`,
+  descriptionAlignment: 'center',
+}
 type NotEligibleEduConnectErrorMessage =
   | 'InvalidAgeFromEduConnect'
   | 'UserAlreadyBeneficiary'
   | 'UserAgeNotValid'
+  | 'InvalidInformation'
 
 export function useNotEligibleEduConnectErrorData(
   message: NotEligibleEduConnectErrorMessage | string
@@ -45,6 +55,9 @@ export function useNotEligibleEduConnectErrorData(
   switch (message) {
     case 'InvalidAgeFromEduConnect':
       return InvalidAgeFromEduConnect
+
+    case 'InvalidInformation':
+      return InvalidInformation
 
     default:
       return UserAgeNotValid
